df1b640d123bf0173a19977fbfbc561f4d910b47
Pankaj Kumar <pankajkumar11thmay2005@gmail.com>
2026-08-26 18:54:31 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello pnkj";
+  return 0;
+}
---
+++ q9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string name ;
+  int age;
+  char grade ;
+  double height;
+
+  cin >> name >> age >> grade >> height;
+
+  cout << "Name: " <<  name << "\n" << "Age: " << age << "\n" <<  "Grade: " << grade <<"\n" <<  "Height: " << height << "\n";
+
+  return 0;
+
+}
