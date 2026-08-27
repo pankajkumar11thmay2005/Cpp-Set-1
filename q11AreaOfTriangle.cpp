c3922f4063080a620b91438bba5d79ea7d615005
Pankaj Kumar <pankajkumar11thmay2005@gmail.com>
2026-08-27 18:14:41 +0530

    Add files via upload

---
+++ q11AreaOfTriangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double radius;
+  cin >> radius;
+
+  double area = 3.14 * radius * radius;
+
+  cout << "Area of Triangle: " << area;
+
+  return 0;
+}
---
+++ q13CompleteSentence.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string sentence;
+
+  getline(cin, sentence);
+
+  cout << sentence;
+
+  return 0;
+}
